e195f71d394dc12f16597fb3f217d9e35f9299e2
M.K3N <kenhasbeenused@gmail.com>
2016-04-19 09:37:48 +0800

    Removed API Code

--- Includes/js/app.js
+++ Includes/js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	// Date Section
 	// Reference http://jsfiddle.net/ipr101/X9hyZ/
 	var monthNames = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun",
 		"Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
@@ -8,7 +9,8 @@ $(document).ready(function(){
 	newDate.setDate(newDate.getDate());    
 	$('.Date').html("<strong>" + dayNames[newDate.getDay()] + "</strong> " + monthNames[newDate.getMonth()] + ' ' + newDate.getDate() + ' ,' + newDate.getFullYear());
 
-	var getWeather = $.get( "http://api.openweathermap.org/data/2.5/weather?q=hk&units=metric&appid=13c81a68eb47d0441a26b3f74543f2c2", "json" );
+	// Top Weather DIV
+	var getWeather = $.get( "http://api.openweathermap.org/data/2.5/weather?q=hk&units=metric", "json" );
 	getWeather.done(function(data){
 		var tempinc = data.main.temp;
 		tempinc = parseFloat(tempinc).toFixed(0);
@@ -20,7 +22,9 @@ $(document).ready(function(){
 		$('.Weather').html(icon+tempinc);
 	});
 	
+	// Weather Box DIV
 	function ForecastItem(date, icon, weather, tmin, tmax){
+		// Get Data to Form a Weather Card
 		var html = "";
 		html += '<div class="fc-items"><div class="thumbnail">';
 		html += '<img src="http://openweathermap.org/img/w/'+icon+'.png" alt="'+weather+'" title="'+weather+'" />';
@@ -33,6 +37,7 @@ $(document).ready(function(){
 	}
 	
 	function dateConverter(UNIX_timestamp){
+		// Convert UNIX Timestamp to Human Friendly Text
 		var ipt = new Date(UNIX_timestamp * 1000);
 		var month = ipt.getMonth()+1;
 		var date = ipt.getDate();
@@ -42,7 +47,7 @@ $(document).ready(function(){
 	}
 	
 	if($('.Forecast').length > 0){
-		var getForecast = $.get( "http://api.openweathermap.org/data/2.5/forecast/daily?q=HongKong&units=metric&cnt=16&appid=13c81a68eb47d0441a26b3f74543f2c2", "json" );
+		var getForecast = $.get( "http://api.openweathermap.org/data/2.5/forecast/daily?q=HongKong&units=metric&cnt=16", "json" );
 		getForecast.done(function(data){
 			var html = "";
 			
